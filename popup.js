a7dd52560561c079e6ad5f8d74142fd091b1fac2
Eitantan <56890550+Eitantan@users.noreply.github.com>
2020-07-19 13:48:18 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var runAPIButton = document.getElementById('runAPI');
+  checkPageButton.addEventListener('click', function() {
 
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      var f = d.createElement('form');
+      f.action = 'https://Extension-Page.eitanalperstein.repl.co';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab.url;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  }, false);
+}, false);
